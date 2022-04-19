2e6d98c1ed9df7c5ede5c4d7b9d6e4527820bafd
patilsayali9822 <92665023+patilsayali9822@users.noreply.github.com>
2022-04-19 22:25:32 +0530

    First Commit

---
+++ javapro1/src/firstpro1/first1.java
@@ -0,0 +1,25 @@
+package firstpro1;
+
+public class first1 {
+	int yearofpurchase;
+	String model;
+	double cost;
+	
+	public void db1 ()
+	{
+		System.out.println("year of purchase = "+2008);
+	}
+	public void db2 ()
+	{
+		System.out.println("model = "+1990);
+		System.out.println("cost = "+400000);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		first1 hey = new first1 ();
+		hey.db1();
+		hey.db2();
+
+	}
+
+}
