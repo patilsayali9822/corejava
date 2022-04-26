52963124aa80d178af1c2ebda810025a18461d1a
patilsayali9822 <92665023+patilsayali9822@users.noreply.github.com>
2022-04-26 18:33:10 +0530

    First Commit

---
+++ javapro1/src/firstpro2/Satatic_variable.java
@@ -0,0 +1,17 @@
+package firstpro2;
+
+public class Satatic_variable {
+	
+static int age;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Satatic_variable m=new Satatic_variable ();
+		Satatic_variable n=new Satatic_variable ();
+		m.age= 40;
+		n.age= 60;
+		System.out.println("age is "+m.age);
+		System.out.println("age is "+n.age);
+	}
+
+}
