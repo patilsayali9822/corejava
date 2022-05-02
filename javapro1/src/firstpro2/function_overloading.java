c94b605a889c604ad1f2d03845c77199124e9c18
patilsayali9822 <92665023+patilsayali9822@users.noreply.github.com>
2022-05-02 20:02:51 +0530

    First Commit

---
+++ javapro1/src/firstpro2/function_overloading.java
@@ -0,0 +1,20 @@
+package firstpro2;
+
+public class function_overloading {
+	int add (int a, int b)
+	{
+		return a + b;
+	}
+	int add (int a, int b, int c)
+	{
+		return a+b+c;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		function_overloading obj = new function_overloading();
+		System.out.println(obj.add(5, 8));
+		System.out.println(obj.add(5, 8, 7));
+
+	}
+
+}
